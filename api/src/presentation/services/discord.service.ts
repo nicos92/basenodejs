e636e49330f79b5f1de9c5bb49b503c01e4c652f
nicoswsl <nicolassandoval140692@gmail.com>
2026-02-15 16:07:12 -0300

    feat: Add GIF to Discord notifications
    Includes a fun GIF in Discord notifications via an embed, enhancing
    visual appeal.

--- api/src/presentation/services/discord.service.ts
+++ api/src/presentation/services/discord.service.ts
@@ -1,26 +1,33 @@
 import { envs } from "../../config";
 
 export class DiscordService {
-  private readonly discordWebhookUrl: string;
-  constructor() {
-    this.discordWebhookUrl = envs.DISCORD_WEBHOOK_URL;
-  }
-
-  async notity(message: string) {
-    const body = {
-      content: message,
-    };
-
-    const response = await fetch(this.discordWebhookUrl, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(body),
-    });
-
-    if (!response.ok) {
-      return false;
+    private readonly discordWebhookUrl: string;
+    constructor() {
+        this.discordWebhookUrl = envs.DISCORD_WEBHOOK_URL;
     }
 
-    return true;
-  }
+    async notity(message: string) {
+        const body = {
+            content: message,
+            embeds: [
+                {
+                    image: {
+                        url: "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXNrcXJuOTY0azg2bnF4MnkybnYzY2V4cWp4cHgwZHJlNDV3cW42cyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/14jzZv9EFK8T8Q/giphy.gif",
+                    },
+                },
+            ],
+        };
+
+        const response = await fetch(this.discordWebhookUrl, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+        });
+
+        if (!response.ok) {
+            return false;
+        }
+
+        return true;
+    }
 }
